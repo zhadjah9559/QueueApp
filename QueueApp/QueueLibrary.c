e36144256a7165070bb0ffeed96e0ff3c02062c5
zhadjah9559 <zhadjah@gmail.com>
2018-09-28 18:00:54 -0400

    Merge origin/master

--- QueueApp/QueueLibrary.c
+++ QueueApp/QueueLibrary.c
@@ -1,21 +1,73 @@
 #include <stdio.h>
-int Enqueque()
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool Enqueque(int queue[], int* front, int* rear, int* nElements, int* capacity, 
+              int userInput)
+{   
+    //if there is no more space in the Queue
+    if( *nElements == capacity )
+    {
+        //update the capacity
+        capacity = *nElements * 2;
+        
+        
+        int* growQueue = (int*)malloc( (*capacity) * sizeof(int) );
+                
+        if(growQueue == NULL)
+            return false;
+        for(int i =0; i<*nElements; i++)
+            growQueue[i] = (*queue)[i];
+       
+        free(*queue);
+        *queue = growQueue;
+        
+        *queue[*nElements] = userInput;
+        nElements++;
+        
+    }
+        
+    //if there is space in the queue
+    else if(*nElements+1 > capacity)
+    {
+        //if queue is empty
+         if(*front == NULL)
+        {
+            //front and rear are both pointers to the same element
+            front = *queue[0];
+            rear = *queue[0];
+            
+            *queue[0] = userInput;
+            nElements++;
+            return true;
+        }
+         
+         else if(nElements >= 1)
+         {
+             //move the userInput into the next available spot behind the 
+             //element after rear. plus four because int takes 4 bytes
+             *queue[ rear+4 ] = userInput;
+             rear = *queue[ rear+4 ];
+             return true;
+         }
+    }
+    
+    
+    
+}
+
+bool Dequeque(int queue[], int* front, int* rear)
 {
 
 }
 
-int Dequeque()
-{
-
-}
-
-int GetCurrentSize()
+int GetCurrentSize(int queue[], int* front, int* rear)
 {
     
 }
 
-int toString()
+int toString(int queue[], int* front, int* rear)
 {
-
+    
 }
 
--- QueueApp/main.cpp
+++ QueueApp/main.cpp
@@ -1,44 +1,67 @@
 #include <stdio.h>
-#include <queue>
+#include <stdlib.h>
+
+bool Enqueque(int queue[], int* front, int* rear, int intialSize);
+bool Dequeque(int queue[], int* front, int* rear);
+int GetCurrentSize(int queue[], int* front, int* rear);
+int toString(int queue[], int* front, int* rear);
+
 int main()
 {
-    bool enqueueWorks, dequeueWorks;
-    
-    int *p;
-    p = (int*)malloc(sizeof(int));
-    
+    bool enqueueResult, dequeueResult, ifEmpty;
     int UserOption = 0;  
+    int capacity = 10;
+        
+    int* queue = (int*)malloc( capacity * sizeof(int) );
+    for(int i = 0; i<capacity; i++)
+        queue[i] = 0;
     
-    printf("This program uses Queues")
+    int* front, rear;
+   
+    
+    printf("This program implements Queues using structured programming. Enter "
+            "a number from 1 to 4 . \n"
+            "1. To enqueue a number \n"
+            "2. To dequeue a number \n"
+            "3. To get the current size of the queue \n"
+            "4. To see the contents within the queue \n");
+    scanf( "%d", &UserOption );
     
     switch(UserOption)
     {
         case 1:
-            enqueueWorks = Enqueque();
+            enqueueResult = Enqueque(&queue, &front, &rear, capacity);
 
-            if(enqueueWorks == true)
+            if(enqueueResult == true)
                 printf( "The number has been put into the queue!");
             else
                 printf("The number was not able to be enqueued!");
             break;
 
         case 2:
-            dequeueWorks = Dequeque();
+            dequeueResult = Dequeque( queue, &front, &rear );
 
-            if(dequeueWorks == true)
-                printf( "The number has been put into the queue!");
+            if(dequeueResult == true)
+                printf( "The number has been dequeued!");
             else
-                printf("The number was not able to be enqueued!");
+                printf("The number wasn't able to be dequeued!");
             break;
 
         case 3:
-            printf( "The current size of the queue is: " GetCurrentSize() );
+            printf( "The current size of the queue is: " + GetCurrentSize( queue, &front, &rear) );
             break;
-
+            
+        //come back to this************************
         case 4:
-            printf( "The contents of the queue are: " toString() );
+            if(ifEmpty == true)
+                printf("Queue is currently empty");
+            else
+                printf( "The contents of the queue are: " toString() );
             break;
+        default:
+            printf("Error, please enter a number from 1 through 4.");
     }
+    free(queue);
     return 0;
     
 }
